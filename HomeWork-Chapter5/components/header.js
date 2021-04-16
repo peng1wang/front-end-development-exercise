4b26b123b74d218d05ce13aa41e363a3b181b545
peng1wang <62434079+peng1wang@users.noreply.github.com>
2021-04-16 23:23:11 +0800

    Create header.js

---
+++ HomeWork-Chapter5/components/header.js
@@ -0,0 +1,9 @@
+function headers(){
+  return <div class="top">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>今日头条-王鹏</title>
+    <link href="style/toutiao.css" type="text/css" rel="stylesheet">
+}
+export default headers
