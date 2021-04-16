e0b8fac9635154fb3d16eeadbacb648839ae6a11
peng1wang <62434079+peng1wang@users.noreply.github.com>
2021-04-16 23:25:23 +0800

    Create app.js

---
+++ HomeWork-Chapter5/pages/app.js
@@ -0,0 +1,18 @@
+import '../style/toutiao.css'
+
+import Headers from '../components/headers'
+import Index from '../components/center'
+
+function MyApp({ Component, pageProps }) {
+  // return <Component {...pageProps} />
+  return (<div>
+    <Headers class="top"></Headers>
+    <div class="overlool">
+    < div class="center">
+    </div>
+
+    </div>
+    </div>
+)
+}
+export default MyApp
